e0d211888344b3d2c3d66a5332d96fe50d97c12a
cjs8487 <cjschaefer1999@gmail.com>
2021-08-11 20:02:22 -0400

    basic websocket setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+import OBSWebSocket from 'obs-websocket-js';
+
+type AppProps = {}
+type AppState = {
+    obsSocket: OBSWebSocket
+}
+
+class App extends React.Component<AppProps, AppState> {
+    
+    constructor(props: AppProps) {
+        super(props);
+        const obsSocket = new OBSWebSocket();
+        this.state = {
+            obsSocket
+        }
+    }
+
+    async componentDidMount() {
+        const { obsSocket } = this.state;
+        //need to await the connection here to ensure that it is connected when we start assembling state data
+        await obsSocket.connect({ address: 'localhost:4444', password: 'password' })
+        obsSocket.send('GetSceneList')
+            .then((scenes) => {
+                console.log(scenes);
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                
+            </div>
+        );
+    }
 }
 
 export default App;
