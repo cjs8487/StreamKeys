a2ccaff64d21de57671d38e51c1ef1b7b99bc54d
cjs8487 <cjschaefer1999@gmail.com>
2021-10-01 01:12:21 -0400

    basic post function for changing matrix

--- src/server.ts
+++ src/server.ts
@@ -10,11 +10,13 @@ const server = express();
 const buildPath = '../frontend/build';
 const port = 3030;
 
+server.use(express.json());
+
 server.get('/ping', (req: Request, res: Response) => {
     res.status(200).send('Pong');
 });
 
-const serialPort: SerialPort = new Serial('COM3');
+const serialPort: SerialPort = new Serial('COM5');
 serialPort.write('MT00RD0000NT');
 serialPort.read();
 server.get('/serialtest', (req: Request, res: Response) => {
@@ -25,6 +27,19 @@ server.get('/serialtest', (req: Request, res: Response) => {
         ret = ret.toString();
     }
     console.log(ret);
+    res.status(200).send(ret);
+});
+
+server.post('/setoutput', (req: Request, res: Response) => {
+    const { output, input } = req.body;
+    const command = `MT00SW${(`${input}`).padStart(2, '0')}${(`${output}`).padStart(2, '0')}NT`;
+    console.log(command);
+    serialPort.write(command);
+    serialPort.write('MT00RD0000NT');
+    let ret = serialPort.read();
+    if (ret != null) {
+        ret = ret.toString();
+    }
     res.status(200).send(ret);
 });
 
